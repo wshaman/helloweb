689a980f92cb0edcb3aca4b007f246f7b245ae57
Serge Noname <rakoth.android@gmail.com>
2022-01-12 16:26:14 +0300

    Web handling homework

---
+++ cmd/muxing/muxing.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func Start(host string, port int) {
+	router := mux.NewRouter()
+
+	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	host := os.Getenv("HOST")
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		port = 8081
+	}
+	Start(host, port)
+}
